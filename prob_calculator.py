dec5bfe799e6981e46bfb263b460e98123dd371b
Seuafai <49505803+Seuafai@users.noreply.github.com>
2023-09-21 13:52:49 +0000

    fixes

--- prob_calculator.py
+++ prob_calculator.py
@@ -5,19 +5,13 @@ import random
 class Hat:
   import copy
   import random
-
+  
   def __init__(self, **kwargs):
     self.contents = []
-    self.contents = [kwargs]
-    #self.contents = []
-    
-    for key, value in self.contents.items():
-      self.contents.extend([key] * value)  # Get the key from the dictionary
-      value = self.contents[key]  # Get the corresponding value
-    
-# Create a list repeating the key (e.g., "cat") 'value' times
-      self.contents = [key] * value
-    print(self.contents)
+
+    for key, value in kwargs.items():
+      self.contents.extend([key] * value)
+
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
