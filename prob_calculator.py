f84928f6af37f257eae40abbc3f80b36a7c86f72
Seuafai <49505803+Seuafai@users.noreply.github.com>
2023-09-22 02:21:50 +0000

    fixes

--- prob_calculator.py
+++ prob_calculator.py
@@ -37,5 +37,26 @@ The experiment function should return a probability.
 """
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+  import collections
+  #hat = hat.contents #
+  result_counter = 0 #counts number of matches
+  #loop each experiment?
   
-  pass
+  eballs = [expected_balls]
+  for draw in range(num_experiments):
+    
+    #print(eballs)
+    listballs = hat.draw(num_balls_drawn)
+    #convert listballs into dict so the dicts can be compared using collections.counter().
+    listballs = collections.Counter(listballs) 
+    #subtract expected balls from listballs. if can, then counter +1. else moves on
+    try:
+      result = {key: eballs[key] - listballs.get(key, 0) for key in eballs}
+      #print(result)
+      result_counter += 1
+    except ValueError:
+      print("fuck")
+     
+  #print(result_counter)
+  prob =  result_counter/num_experiments
+  return prob
